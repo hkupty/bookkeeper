a32830bc02d1d79e7b860f60df74acf5a14eda12
Henry Kupty <hkupty@gmail.com>
2014-12-18 15:15:35 -0200

    Update __init__.py

--- bookkeeper/__init__.py
+++ bookkeeper/__init__.py
@@ -9,7 +9,15 @@ simplifying Stow use on dot file management, so, while the enhancements
 here proposed and implemented may be useful for software installation,
 this is by no means the main intent of this project.
 """
+from bookkeeper import persist, cli, core
+
+
 __author__ = "Henry 'Ingvij' Kupty"
 __app__ = "Bookkeeper"
 __version__ = '0.0.1'
 __license__ = 'BSD'
+__all__ = [
+    "persist",
+    "cli",
+    "core"
+]
