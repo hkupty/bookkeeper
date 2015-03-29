1fd8dbcd86b0d9da6dca2133254fd2a2e45f4ba3
Henry Kupty <hkupty@gmail.com>
2015-03-29 12:43:59 -0300

    setup: Solve #1 by reading `__init__.py` as a file

--- setup.py
+++ setup.py
@@ -1,19 +1,25 @@
 # encoding: utf-8
 """ Setup script. """
 from distutils.core import setup
-import bookkeeper
+
+with open("bookkeeper/__init__.py") as f:
+    bookkeeper = {
+        k.split(' = ')[0]: k.split(' = ')[1]
+        for k in f.readlines()
+        if "=" in k
+    }
 
 setup(
-    name=bookkeeper.__app__,
-    version=bookkeeper.__version__,
-    author=bookkeeper.__author__,
+    name=bookkeeper['__app__'],
+    version=bookkeeper['__version__'],
+    author=bookkeeper['__author__'],
     author_email='hkupty@gmail.com',
     description='GNU Stow with steroids',
     long_description=""" Bookkeeper works as a symbolic link manager.
     With some more flexibility, Bookkeeper makes the management of dotfiles easy
     and painless.""",
     scripts=['bin/bk'],
-    license=bookkeeper.__license__,
+    license=bookkeeper['__license__'],
     download_url='http://github.com/hkupty/bookkeeper',
     packages=['bookkeeper'],
     classifiers=[
